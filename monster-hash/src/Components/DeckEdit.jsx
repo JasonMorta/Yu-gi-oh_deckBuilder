8d4c146e52d73775189d3c48e76b190ed0eed1fa
Jason Morta <46291780+JasonMorta@users.noreply.github.com>
2023-10-17 00:14:34 +0200

    updates

--- monster-hash/src/Components/DeckEdit.jsx
+++ monster-hash/src/Components/DeckEdit.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./DeckEdit.css"
 import { useSelector, useDispatch } from "react-redux";
-import { loadCards } from "../redux/cardsState";
+import { loadCards, saveDeck } from "../redux/cardsState";
 import LazyImage from './LazyImage';
+import BasicButtons from './Btn';
 
 export default function DeckEdit() {
 
@@ -10,12 +11,38 @@ export default function DeckEdit() {
     const deck = useSelector((state) => state.cardsState.deck);
     console.log('deck', deck)
 
+  async  function saveDeck() {
+    await fetch("/saveDeck", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(deck)
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        // save cards to redux store
+        console.log('saved:')
+
+      })
+      .catch((error) => {
+        console.table(error)
+      })
+        
+    }
+
   return (
     <>
 
     {/* Deck layout */}
     <div className="deck">
     <h1>Deck</h1>
+    <BasicButtons 
+       onClick={()=> saveDeck()}
+       text={"Save Deck"}
+       variant={"contained"}
+       id={"save-deck-btn"}
+     />
     {deck.map((card) => (
        <>
          <LazyImage 
